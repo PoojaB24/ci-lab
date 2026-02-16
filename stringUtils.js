b82752cbab97b92cfc2c069679c1c2853e39b810
PoojaB24 <poojab.202109@gmail.com>
2026-02-16 12:19:05 +0530

    Update stringUtils.js

--- stringUtils.js
+++ stringUtils.js
@@ -1,9 +1,9 @@
-
 function capitalize(str) {
-return str.charAt(0).toUpperCase() + str.slice(1);
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
 
-}
 function reverse(str) {
-return str.split(&#39;&#39;).reverse().join(&#39;&#39;);
+    return str.split('').reverse().join('');
 }
+
 module.exports = { capitalize, reverse };
